da52448b8a01b879f52813af62932c8afb443edd
Jacob Prebys <jprebys@sgcaptrading.com>
2022-08-15 15:47:10 -0500

    Display window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <errno.h>
 
 #include "X11/Xlib.h"
@@ -14,9 +15,33 @@ void error_and_exit(char *desc)
 
 int main(void)
 {	
-	Display *display = XOpenDisplay(NULL);
+	Display *display;
+	Window window;
+	XEvent event;
+	int screen;
+
+	display = XOpenDisplay(NULL);
 	if (display ==  NULL)
 		error_and_exit("XOpenDisplay");
+
+	screen = DefaultScreen(display);
+
+	window = XCreateSimpleWindow(display, RootWindow(display, screen), 
+		                         0, 0, 600, 400, 10, 
+		                         WhitePixel(display, screen), 
+		                         BlackPixel(display, screen));
+
+    XSelectInput(display, window, ExposureMask | KeyPressMask);
+	XMapWindow(display, window);
+
+	while (true) {
+		XNextEvent(display, &event);
+		if (event.type == Expose) {
+			printf("Hello World\n");
+		} else if (event.type == KeyPress) {
+			break;
+		}
+	}
 
 	printf("Hello World\n");
 
